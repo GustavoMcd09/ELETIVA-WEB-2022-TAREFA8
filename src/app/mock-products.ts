c23433b8dd17968929aa85f1ad54b0356c079041
GustavoMcd09 <gustavosmacedopf@gmail.com>
2022-05-09 21:38:27 -0300

    TAREFA 8

---
+++ src/app/mock-products.ts
@@ -0,0 +1,9 @@
+import { Product } from './products';
+
+export const PRODUCTS: Product[] = [
+  { id: 11, name: '   Produto 1' },
+  { id: 12, name: '   Produto 2' },
+  { id: 13, name: '   Produto 3' },
+  { id: 14, name: '   Produto 4' },
+  { id: 15, name: '   Produto 5' },
+];
